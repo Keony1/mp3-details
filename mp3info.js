c8341566f2ce455821a0e0da9c484a54f76e6d29
Keony Schröer <keony.schroer@gmail.com>
2024-05-24 21:36:12 -0300

    style: linter and fomatter

--- mp3info.js
+++ mp3info.js
@@ -1,33 +1,34 @@
-const fs = require("node:fs");
-const { Buffer } = require("node:buffer");
+const fs = require('node:fs')
+const { Buffer } = require('node:buffer')
 
 class Mp3Info {
   constructor(frameHeader, frameXingHeader) {
-    this.frameHeader = frameHeader;
-    this.frameXingHeader = frameXingHeader;
+    this.frameHeader = frameHeader
+    this.frameXingHeader = frameXingHeader
   }
+
   xingFlags() {
-    return this.frameXingHeader.readUInt32BE(4); // 0x00 0x00 0x00 0x0f this means that the four flags are present in the file
+    return this.frameXingHeader.readUInt32BE(4) // 0x00 0x00 0x00 0x0f this means that the four flags are present in the file
   }
 
   xingFrames() {
-    return this.frameXingHeader.readUInt32BE(8);
+    return this.frameXingHeader.readUInt32BE(8)
   }
 
   mpegVersionBits() {
-    return (this.frameHeader[1] & 0x18) >> 3;
+    return (this.frameHeader[1] & 0x18) >> 3
   }
 
   layerBits() {
-    return (this.frameHeader[1] & 0x6) >> 1;
+    return (this.frameHeader[1] & 0x6) >> 1
   }
 
   protectionBits() {
-    return this.frameHeader[1] & 0x1;
+    return this.frameHeader[1] & 0x1
   }
 
   bitrateBits() {
-    return (this.frameHeader[2] & 0xf0) >> 4;
+    return (this.frameHeader[2] & 0xf0) >> 4
   }
 
   /**
@@ -36,7 +37,7 @@ class Mp3Info {
    */
   getBitrate() {
     const BITRATE_TABLE = {
-      "MPEG 1": [
+      'MPEG 1': [
         [
           0, 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448,
           0,
@@ -44,16 +45,16 @@ class Mp3Info {
         [0, 32, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 384, 0], // layer 2
         [0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, 0], // layer 3
       ],
-      "MPEG 2": [
+      'MPEG 2': [
         // MPEG 2 and MPEG 2.5
         [0, 32, 48, 56, 64, 80, 96, 112, 128, 144, 160, 176, 192, 224, 256, 0], // layer 1
         [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0], // layer 2
         [0, 8, 16, 24, 32, 40, 48, 56, 64, 80, 96, 112, 128, 144, 160, 0], // layer 3
       ],
-    }; // all values are in kbps
-    const version = (this.mpegVersionBits() & 0x01) === 0 ? "MPEG 2" : "MPEG 1";
-    const layerIndex = this.layerBits();
-    const bitrateIndex = this.bitrateBits();
+    } // all values are in kbps
+    const version = (this.mpegVersionBits() & 0x01) === 0 ? 'MPEG 2' : 'MPEG 1'
+    const layerIndex = this.layerBits()
+    const bitrateIndex = this.bitrateBits()
 
     if (
       layerIndex < 0 ||
@@ -61,49 +62,49 @@ class Mp3Info {
       bitrateIndex < 0 ||
       bitrateIndex > 15
     ) {
-      throw new Error("Invalid layer or bitrate index");
+      throw new Error('Invalid layer or bitrate index')
     }
 
-    return BITRATE_TABLE[version][layerIndex][bitrateIndex];
+    return BITRATE_TABLE[version][layerIndex][bitrateIndex]
   }
 
   frequencyBits() {
-    return (this.frameHeader[2] & 0xc) >> 2;
+    return (this.frameHeader[2] & 0xc) >> 2
   }
 
   paddingBits() {
-    return (this.frameHeader[2] & 0x2) >> 1;
+    return (this.frameHeader[2] & 0x2) >> 1
   }
 
   samplesPerFrame() {
-    const layer = this.getLayer();
-    const version = this.getMPEGVersion();
-    if (layer === "Layer I") {
-      return 384;
+    const layer = this.getLayer()
+    const version = this.getMPEGVersion()
+    if (layer === 'Layer I') {
+      return 384
     }
 
-    if (layer === "Layer II") {
-      return 1152;
+    if (layer === 'Layer II') {
+      return 1152
     }
 
-    if (layer === "Layer III") {
-      if (version === "MPEG 1") {
-        return 1152;
+    if (layer === 'Layer III') {
+      if (version === 'MPEG 1') {
+        return 1152
       }
 
       // for versions 2 and 2.5
-      return 576;
+      return 576
     }
   }
 
   getMPEGVersion() {
-    const table = ["MPEG 2.5", "reserved", "MPEG 2", "MPEG 1"];
-    return table[this.mpegVersionBits()];
+    const table = ['MPEG 2.5', 'reserved', 'MPEG 2', 'MPEG 1']
+    return table[this.mpegVersionBits()]
   }
 
   getLayer() {
-    const table = ["reserved", "Layer III", "Layer II", "Layer I"];
-    return table[this.layerBits()];
+    const table = ['reserved', 'Layer III', 'Layer II', 'Layer I']
+    return table[this.layerBits()]
   }
 
   /**
@@ -111,21 +112,21 @@ class Mp3Info {
    */
   getFrequency() {
     const table = [
-      [44100, 48000, 32000, "reserved"], // v1
-      [22050, 24000, 16000, "reserved"], // v2
-      [11025, 12000, 8000, "reserved"], // v2.5
-    ];
+      [44100, 48000, 32000, 'reserved'], // v1
+      [22050, 24000, 16000, 'reserved'], // v2
+      [11025, 12000, 8000, 'reserved'], // v2.5
+    ]
 
-    const freqIdx = this.frequencyBits();
+    const freqIdx = this.frequencyBits()
     switch (this.getMPEGVersion()) {
-      case "MPEG 2.5":
-        return table[2][freqIdx];
-      case "MPEG 2":
-        return table[1][freqIdx];
-      case "MPEG 1":
-        return table[0][freqIdx];
+      case 'MPEG 2.5':
+        return table[2][freqIdx]
+      case 'MPEG 2':
+        return table[1][freqIdx]
+      case 'MPEG 1':
+        return table[0][freqIdx]
       default:
-        throw new Error("not a valid frequency");
+        throw new Error('not a valid frequency')
     }
   }
 
@@ -133,26 +134,23 @@ class Mp3Info {
     // VBR
     // Number of frames * Samples Per Frame  / Sampling Rate (aka frequency)
     if (this.frameXingHeader) {
-      return (
-        (this[xingFrames]() * this.samplesPerFrame()) / this.getFrequency()
-      );
+      return (this.xingFrames() * this.samplesPerFrame()) / this.getFrequency()
     }
     // CBR
     // File Size in bits / bitrate in bps
-
-    return (fileSize * 8) / (this.getBitrate() * 1000);
+    return (fileSize * 8) / (this.getBitrate() * 1000)
   }
 
   formatHHMMSS(duration) {
-    const hours = Math.floor(duration / 3600);
-    const minutes = Math.floor((duration % 3600) / 60);
-    const seconds = Math.floor(duration % 60);
+    const hours = Math.floor(duration / 3600)
+    const minutes = Math.floor((duration % 3600) / 60)
+    const seconds = Math.floor(duration % 60)
 
-    const fHours = String(hours).padStart(2, "0");
-    const fMinutes = String(minutes).padStart(2, "0");
-    const fSeconds = String(seconds).padStart(2, "0");
+    const fHours = String(hours).padStart(2, '0')
+    const fMinutes = String(minutes).padStart(2, '0')
+    const fSeconds = String(seconds).padStart(2, '0')
 
-    return `${fHours}:${fMinutes}:${fSeconds}`;
+    return `${fHours}:${fMinutes}:${fSeconds}`
   }
 }
 
@@ -166,7 +164,8 @@ class Mp3Info {
  * @property {version} version
  */
 
-let fd;
+let fd
+let fileSize
 /**
  * Loads the MP3 metadata (id3 tags not included)
  *
@@ -175,76 +174,76 @@ let fd;
  *         The metadata of the MP3
  */
 function load(path) {
-  fd = fs.openSync(path);
-  fileSize = fs.statSync(path).size;
+  fd = fs.openSync(path)
+  fileSize = fs.statSync(path).size
 
-  let pos = id3Size();
-  const frameHeader = mp3FrameHeader(pos);
+  let pos = id3Size()
+  const frameHeader = mp3FrameHeader(pos)
   if (frameHeader === null) {
-    throw new Error("Not a valid MP3 file");
+    throw new Error('Not a valid MP3 file')
   }
-  pos += frameHeader.length;
-  const frameXingHeader = xingHeader(pos + 10); // 10 is the offset from the headerframe
+  pos += frameHeader.length
+  const frameXingHeader = xingHeader(pos + 10) // 10 is the offset from the headerframe
 
-  const reader = new Mp3Info(frameHeader, frameXingHeader);
-  const duration = reader.getDuration();
-  const bitrate = reader.getBitrate();
-  const layer = reader.getLayer();
-  const version = reader.getMPEGVersion();
-  const frequency = reader.getFrequency();
+  const reader = new Mp3Info(frameHeader, frameXingHeader)
+  const duration = reader.getDuration()
+  const bitrate = reader.getBitrate()
+  const layer = reader.getLayer()
+  const version = reader.getMPEGVersion()
+  const frequency = reader.getFrequency()
 
-  return { duration, bitrate, frequency, layer, version };
+  return { duration, bitrate, frequency, layer, version }
 }
 
 function xingHeader(pos) {
-  const IDENTIFIER_SIZE = 4;
-  const FLAGS_SIZE = 4;
-  const FRAMES_SIZE = 4;
-  const BYTES_SIZE = 4;
-  const TOC_SIZE = 100;
-  const VBR_SIZE = 4;
+  const IDENTIFIER_SIZE = 4
+  const FLAGS_SIZE = 4
+  const FRAMES_SIZE = 4
+  const BYTES_SIZE = 4
+  const TOC_SIZE = 100
+  const VBR_SIZE = 4
 
-  let buffer = Buffer.alloc(40);
-  fs.readSync(fd, buffer, 0, buffer.length, pos);
-  let size = 0;
-  let offset = 0;
+  let buffer = Buffer.alloc(40)
+  fs.readSync(fd, buffer, 0, buffer.length, pos)
+  let size = 0
+  let offset = 0
 
   for (let i = 0; i < buffer.length - IDENTIFIER_SIZE; i++) {
     // If size is greater than 0, it means that "Xing" or "Info" identifier was found
-    if (size) break;
+    if (size) break
 
-    let identifier = buffer.toString("ascii", i, i + IDENTIFIER_SIZE);
-    if (identifier === "Info" || identifier === "Xing") {
-      size += IDENTIFIER_SIZE + FLAGS_SIZE;
-      offset = i;
-      const flags = buffer.readUInt32BE(i + 4);
+    let identifier = buffer.toString('ascii', i, i + IDENTIFIER_SIZE)
+    if (identifier === 'Info' || identifier === 'Xing') {
+      size += IDENTIFIER_SIZE + FLAGS_SIZE
+      offset = i
+      const flags = buffer.readUInt32BE(i + 4)
 
       // frame flag
       if (flags & 0x01) {
-        size += FRAMES_SIZE;
+        size += FRAMES_SIZE
       }
 
       // bytes flag
       if (flags & 0x02) {
-        size += BYTES_SIZE;
+        size += BYTES_SIZE
       }
 
       // TOC flag
       if (flags & 0x04) {
-        size += TOC_SIZE;
+        size += TOC_SIZE
       }
 
       // VBR scale flag
       if (flags & 0x08) {
-        size += VBR_SIZE;
+        size += VBR_SIZE
       }
     }
   }
 
   // Didn't find xing/info tags
-  if (!size) return;
+  if (!size) return
 
-  return Buffer.copyBytesFrom(buffer, offset, size);
+  return Buffer.copyBytesFrom(buffer, offset, size)
 }
 
 /**
@@ -252,14 +251,14 @@ function xingHeader(pos) {
  * @returns {Buffer | null} The MP3 frame header bytes, returns null if not found
  */
 function mp3FrameHeader(pos) {
-  const buffer = Buffer.alloc(4);
-  fs.readSync(fd, buffer, 0, buffer.length, pos);
+  const buffer = Buffer.alloc(4)
+  fs.readSync(fd, buffer, 0, buffer.length, pos)
 
   if ((buffer[0] === 0xff) & ((buffer[1] & 0xc0) === 0xc0)) {
-    return buffer;
+    return buffer
   }
 
-  return null;
+  return null
 }
 
 /**
@@ -267,17 +266,17 @@ function mp3FrameHeader(pos) {
  * @returns the size in bytes of the ID3 segment
  */
 function id3Size() {
-  const HEADER_SIZE = 10;
-  const buffer = Buffer.alloc(HEADER_SIZE);
-  fs.readSync(fd, buffer);
+  const HEADER_SIZE = 10
+  const buffer = Buffer.alloc(HEADER_SIZE)
+  fs.readSync(fd, buffer)
 
-  if (buffer.toString("utf8", 0, 3) !== "ID3") {
-    return 0;
+  if (buffer.toString('utf8', 0, 3) !== 'ID3') {
+    return 0
   }
 
-  const headerSize = 10;
+  const headerSize = 10
   if (buffer.length < headerSize) {
-    return 0;
+    return 0
   }
 
   // The size flag doesn't sum to its value the ID3 Header size
@@ -288,9 +287,9 @@ function id3Size() {
       ((buffer[8] & 0x7f) << 7) |
       (buffer[9] & 0x7f)) +
     HEADER_SIZE
-  );
+  )
 }
 
 module.exports = {
   load,
-};
+}
